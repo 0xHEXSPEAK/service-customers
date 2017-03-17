74d8ed51c131d04c336d91a3d20c17545379e2ed
devyk <hortuk@yahoo.com>
2017-03-18 00:28:55 +0200

    Cache call integration for index action.

--- api/modules/api/v1/controllers/CountryController.php
+++ api/modules/api/v1/controllers/CountryController.php
@@ -4,6 +4,7 @@ namespace api\modules\api\v1\controllers;
 
 use yii;
 use api\modules\api\v1\models\Country;
+use cheatsheet\Time;
 
 /**
  * CountryController
@@ -17,9 +18,25 @@ class CountryController extends BaseController
      */
     public $modelClass = 'api\modules\api\v1\models\Country';
 
+    public function actions()
+    {
+        return [];
+    }
+
     public function actionStates($id)
     {
         return $this->findCountry($id)->states;
+    }
+
+    public function actionIndex()
+    {
+        $page = Yii::$app->request->getQueryParam('page');
+        return Yii::$app->cache->getOrSet('customer-countries-'.$page, function() {
+            $dataProvider = new yii\data\ActiveDataProvider([
+                'query' => Country::find()
+            ]);
+            return $dataProvider->getModels();
+        }, Time::SECONDS_IN_A_MONTH);
     }
 
     public function findCountry($id)
